fd1e7f28aba6b11a708ad8018980cd19bbea1551
Cameron Ho <ho.cameron98@gmail.com>
2025-04-09 16:24:35 -0700

    edited script

--- scripts/exploratory/daily highcount camera.R
+++ scripts/exploratory/daily highcount camera.R
@@ -1,2 +1,9 @@
 #creating daily high counts from the cougar kill cameras 
+#prep data by creating an artificial smapling interval
+#look for a photo taken within +- 2 min of a 10 min interval
 
+library(data.table)
+
+file_names <- list.files("data/cougar_cam_data_files")[grep("csv", list.files("data/cougar_cam_data_files"))]
+
+#need to be careful with 23-302 since there are also ground observations
