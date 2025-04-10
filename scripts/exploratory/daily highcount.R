61ef85ea6e7c051557ece6ba1aed86522ab54930
Cameron Ho <ho.cameron98@gmail.com>
2025-04-10 12:09:21 -0700

    moved functions to seperate scripts and sourced

--- scripts/exploratory/daily highcount.R
+++ scripts/exploratory/daily highcount.R
@@ -43,112 +43,33 @@ data$dod <- mdy(data$dod)
 data$delta_dod <- as.numeric(data$date - data$dod)
 
 
-### Getting combined counts between spatial areas during a observation period -----------------------------
-  
-  obs_to_clean <- function(data, species){
-    #subsetting to desired species
-    tmp_data <- data[data$scav_species == species,]
+### reading functions in -----------------------------
+#combine count areas
+source("scripts/combine_area_function.R")
+
+#extract day high count
+source("scripts/daily_high_function.R")
     
-    #creating a variable length list to put unique date/time combinations into
-    dattime <- unique(tmp_data[,-c(4,5)])
-    dattime_list <- vector("list", length = nrow(dattime))
-    
-    
-    #placing unique date/time rows into their designated list spot
-    for(i in 1:nrow(dattime)){
-      dattime_list[[i]] <- tmp_data[tmp_data$date == dattime[i,]$date & 
-                                  tmp_data$time == dattime[i,]$time &
-                                  tmp_data$kill_num == dattime[i,]$kill_num,]
-    }
-    
-    
-    #calculating the total count for each datetime
-    #and creating new data frame with new combined area counts
-    dattime$count <- unlist(lapply(dattime_list, function(x){sum(x$number_of_animals)}))
-    
-    
-    ## separating into a list by kill number
-    mort <- unique(dattime$kill_num)
-    mort_list <- vector("list", length = length(mort))
-    
-    for(i in 1:length(mort_list)){
-      mort_list[[i]] <- dattime[dattime$kill_num == mort[i],]
-    }
-    
-    #calculating the max count for each deltadod
-    #if multiple observations in a day have the same max count, only taking one of them
-    mort_list_max <- lapply(mort_list, function(x) {
-      ddd <- unique(x$delta_dod)
-      
-      ddd_high <- slice(x, 0)
-      
-      for (i in 1:length(ddd)) {
-        ddd_high <- filter(x, delta_dod == ddd[i]) %>%
-          filter(count == max(count)) %>%
-          slice(1) %>%
-          bind_rows(ddd_high)
-        
-      }
-      return(ddd_high)
-    })
-    
-    
-    #fixing bug where days observed, but without a count for a species doesn't pull a zero
-      #because there is no associated data point
-    
-    obs_period <- data %>% 
-      group_by(kill_num) %>% 
-      dplyr::summarize(min = min(date), max = max(date))
-    
-    mort_df <- do.call("rbind", lapply(mort_list_max, function(x) {
-      obs_dates <- obs_period %>% 
-        filter(kill_num == x[1,"kill_num"])
-      obs_dates <- seq(obs_dates$min, obs_dates$max, 'days')
-      
-      if(length(obs_dates) != nrow(x)){
-        missing <- obs_dates[-which(obs_dates %in% x$date)]
-        for(i in 1:length(missing)){
-          x <- rbind(x, data.frame(
-                          kill_num = x[1,]$kill_num,
-                          date = missing[i],
-                          time = NA,
-                          number_of_animals = 0,
-                          dod = x[1,]$dod,
-                          species = x[1,]$species,
-                          sex = x[1,]$sex,
-                          age_class = x[1,]$age_class,
-                          cougar_kill = x[1,]$cougar_kill,
-                          delta_dod = missing[i] - x[1,]$dod,
-                          count = 0))
-        }
-      }
-      return(x)
-    }))
-    
-    return(mort_df)
-  }
-  
+
+# transforming data -----------------------------------------------------------
+
     #' running function
     #' making sure all rows have the necessary data
       #' mort #
       #' prey species & age
       #' delta_dod
       #' count
-    wolf_data <- obs_to_clean(data, "WOLF")
+    wolf_data <- daily_high_count(obs_combine_area(data, "WOLF"))
     wolf_dattime <- unique(data[data$scav_species == "WOLF" & data$delta_dod %in% unique(wolf_data$delta_dod), c(1,5,7:9)])
-    raven_data <- obs_to_clean(data, "RAVEN")
+    raven_data <- daily_high_count(obs_combine_area(data, "RAVEN"))
     raven_dattime <- unique(data[data$scav_species == "RAVEN" & data$delta_dod %in% unique(raven_data$delta_dod), c(1,5,7:9)])
-    magpie_data <- obs_to_clean(data, "MAGPIE")
+    magpie_data <- daily_high_count(obs_combine_area(data, "MAGPIE"))
     magpie_dattime <- unique(data[data$scav_species == "MAGPIE" & data$delta_dod %in% unique(magpie_data$delta_dod),c(1,5,7:9)])
-    coyote_data <- obs_to_clean(data, "COYOTE")
+    coyote_data <- daily_high_count(obs_combine_area(data, "COYOTE"))
     coyote_dattime <- unique(data[data$scav_species == "COYOTE" & data$delta_dod %in% unique(coyote_data$delta_dod),c(1,5,7:9)])
-    baea_data <- obs_to_clean(data, "BALD EAGLE")
+    baea_data <- daily_high_count(obs_combine_area(data, "BALD EAGLE"))
     baea_dattime <- unique(data[data$scav_species == "BALD EAGLE" & data$delta_dod %in% unique(baea_data$delta_dod),c(1,5,7:9)])
-    goea_data <- obs_to_clean(data, "GOLDEN EAGLE")
+    goea_data <- daily_high_count(obs_combine_area(data, "GOLDEN EAGLE"))
     goea_dattime <- unique(data[data$scav_species == "GOLDEN EAGLE" & data$delta_dod %in% unique(goea_data$delta_dod),c(1,5,7:9)])
   
-    
-###23-051 is messed up and not linking to the correct kill
-    #the kill wasn't entered into the database
-    #unknown deer
     
